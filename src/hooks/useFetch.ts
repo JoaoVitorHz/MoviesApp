b7204f4823bf2e10d4817bd2abe09fb115603bf8
João Vitor <vitorjoao39207@gmail.com>
2023-06-26 14:05:42 -0300

    refatorando o useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,44 +2,31 @@ type typeData = {
     backdrop_path: string;
 }
 
+function FormateUrlImage(data: any) {
+    data.results.forEach((item: typeData) => {
+        item.backdrop_path =  "https://image.tmdb.org/t/p/original" + item.backdrop_path;
+    });
+    return data;
+}
+
 export const request = {
-    getData: async (selectedAction?: string) => {
+    getData: async (selectedAction: number = 1) => {
 
-        let action;
-        switch(selectedAction){
-            case '1':
-                action = "movie/now_playing";
-                break;
-            case '2':
-                action = "movie/popular";
-                break;
-            case '3':
-                action = "movie/top_rated";
-                break;
-            case '4':
-                action = "tv/popular";
-                break;
-            case '5':
-                action = "tv/popular";
-                break;
-            case '6':
-                action = "tv/airing_today";
-                break;
-            case '7':
-                action = "tv/top_rated";
-                break;
-
-            default: 
-                action = "movie/now_playing";
-                break;
+        const getAction:any = {
+            1: "movie/now_playing",
+            2: "movie/popular",
+            3: "movie/top_rated",
+            4: "tv/popular",
+            5: "tv/popular",
+            6: "tv/airing_today",
+            7: "tv/top_rated"
         }
+
+        let action = getAction[selectedAction] ?? "movie/now_playing"
 
         let respone = await fetch("https://api.themoviedb.org/3/" + action + "?api_key=8cdeb4dea31d00d9f79772db9c9fa4a2&language=pt-br&page=1");
         let json = await respone.json();
 
-        json.results.forEach((data: typeData) => {
-            data.backdrop_path =  "https://image.tmdb.org/t/p/original" + data.backdrop_path;
-        });
-        return json;
+        return FormateUrlImage(json);
     }
 }
